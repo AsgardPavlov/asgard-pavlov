ab776ba6852daf0a3b7242c718340346df53c62c
AsgardPavlov <pasgard9@gmail.com>
2025-03-21 15:04:40 +0500

    fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,42 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  output: 'standalone'
+  output: 'standalone',
+
+  async headers() {
+    return [
+      {
+        source: '/.well-known/acme-challenge/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+        ],
+      },
+    ]
+  }
 };
 
 export default nextConfig;
